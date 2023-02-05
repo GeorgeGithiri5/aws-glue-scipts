f4008244affc08cc9f06ea40ac2dc9cb30f87688
GeorgeGithiri5 <georgegithiri002@gmail.com>
2023-02-05 09:31:03 +0300

    Feature: JDBCSalesforce Scala

---
+++ GlueCustomConnectors/gluescripts/withConnection/JDBCSalesforce.scala
@@ -0,0 +1,38 @@
+object Salesforcejdbc {
+    def main(sysArgs: Array[String]){
+        val conf = new SparkConf.setAppName("JDBCSalesforce").setMaster("local")
+        val spark: SparkContext = new SparkContext(conf)
+        val glueContext: GlueContext = new GlueContext(spark)
+        val rootLogger = Logger.getRootLogger()
+        rootLogger.setLevel(Level.ERROR)
+
+        val optionsMap = Map(
+            "dbTable" -> "Account",
+            "partitionColumn" -> "RecordId__c",
+            "lowerBound" -> "0",
+            "upperBound" -> "123",
+            "numPartitions" -> "6",
+            "connectionName" -> "my-connection"
+        )
+
+        val customSource = glueContext.getSource(
+            connectionType = "custom.jdbc",
+            connectionOptions = JsonOptions(optionsMap),
+            transformationContext = ""
+        )
+        val dyf = customSource.getDynamicFrame()
+        dyf.printSchema()
+        dfy.show()
+
+        val sinkOptionsMap = Map(
+            "dbtable" -> "Account",
+            "connectionName" -> "my-connection"
+        )
+
+        val customSink = glueContext.getSink(
+            connectionType = "custom.jdbc",
+            connectionOptions = JsonOptions(sinkOptionsMap)
+        )
+        customSink.writeDynamicFrame(dfy)
+    }
+}
